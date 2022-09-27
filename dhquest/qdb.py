9650a130c7e16fec46be5036442a3bfa41be36e9
kzk2000 <kilianm@gmail.com>
2022-09-27 19:16:21 -0400

    tactical fix to ensure Pandas string columns become Java strings

--- dhquest/qdb.py
+++ dhquest/qdb.py
@@ -1,8 +1,8 @@
-import deephaven.dtypes as dht
-from deephaven import kafka_consumer as ck
-from deephaven import pandas as dhpd  # this assumes you are running from DH UI site, otherwise this will throw
 import pandas as pd
 import psycopg2
+from deephaven import dtypes, new_table
+from deephaven import numpy as dhnp
+from deephaven import pandas as dhpd  # this assumes you are running from DH UI site, otherwise this will throw
 
 
 def get_connection():
@@ -14,9 +14,29 @@ def get_connection():
         database='qdb')
 
 
+def to_table(df):
+    """
+    This is just a variant of deephaven.pandas.to_table() to always cast the 'object' from
+    input df as Java string.
+    """
+    cols = list(df)
+
+    input_cols = []
+    for col in cols:
+        np_array = df.get(col).values
+        dtype = dtypes.from_np_dtype(np_array.dtype)
+        if dtype is dtypes.PyObject:
+            dtype = dtypes.string
+
+        np_array = dhpd._map_na(np_array)
+        input_cols.append(dhnp._make_input_column(col, np_array, dtype))
+
+    return new_table(cols=input_cols)
+
+
 def run_query(query):
     with get_connection() as conn:
-        return dhpd.to_table(pd.read_sql_query(query, conn))
+        return to_table(pd.read_sql_query(query, conn))
 
 
 def get_trades(last_nticks=1000, verbose=False):
@@ -49,4 +69,3 @@ def get_candles(sample_by='5s', verbose=False):
         print(query)
 
     return run_query(query)
-
